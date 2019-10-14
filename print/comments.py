8f4291624451e979973abe73cffe3acfa9c36abe
Santiago <sanfctz@gmail.com>
2019-10-13 23:43:12 -0300

    Units 1 - 12

---
+++ print/comments.py
@@ -0,0 +1,4 @@
+#this line of code does nothing
+print('Hello')
+# print('hello it's a small world')
+print("hello it's a small world")
---
+++ print/hello-world.py
@@ -0,0 +1,5 @@
+#You can use single or double quotes
+print('Hello\nWorld!')
+#Input: function that received the input from the user
+name = input('What is your name?')
+print('Hello ' + name)
