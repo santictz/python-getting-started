1fa0698cc10d02755f8afaefb42d755f3c668894
Santiago <sanfctz@gmail.com>
2019-10-13 23:57:12 -0300

    Unit 12 - 14

---
+++ numbers/numbers.py
@@ -0,0 +1,10 @@
+pi = 3.14159
+print(pi)
+
+first_num = int(input('Please enter the first number\n'))
+second_num = int(input('Please enter the second number\n'))
+#Potencia
+print(first_num ** second_num)
+
+days_in_february = 28
+print(str(days_in_february) + ' total days in February')
